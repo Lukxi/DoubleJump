f1f39b8c648cd96db980a7868646756570abadbd
Lukxi <nowak.lukas03@outlook.de>
2023-09-16 01:35:52 +0200

    implemented listener

--- src/main/java/me/lukxi/doublejump/listeners/JumpListener.java
+++ src/main/java/me/lukxi/doublejump/listeners/JumpListener.java
@@ -1,4 +1,55 @@
 package me.lukxi.doublejump.listeners;
 
-public class JumpListener {
+import org.bukkit.GameMode;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.*;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class JumpListener implements Listener {
+    private static HashMap<UUID, Boolean> cooldown = new HashMap<>();
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e){
+        if (e.getPlayer().hasPermission("doublejump.use")){
+            e.getPlayer().setAllowFlight(true);
+            cooldown.put(e.getPlayer().getUniqueId(), false);
+        }
+    }
+
+
+    @EventHandler
+    public void onLeave(PlayerQuitEvent e){
+        if (!e.getPlayer().hasPermission("doublejump.use")) return;
+        cooldown.remove(e.getPlayer().getUniqueId());
+    }
+
+    @EventHandler
+    public void onFlightActivation(PlayerToggleFlightEvent e){
+        if (e.getPlayer().hasPermission("doublejump.use") && e.getPlayer().getGameMode() == GameMode.SURVIVAL){
+            e.setCancelled(true);
+            if (!cooldown.get(e.getPlayer().getUniqueId())){
+                e.getPlayer().setVelocity(e.getPlayer().getLocation().getDirection().setY(1.5));
+                cooldown.put(e.getPlayer().getUniqueId(), true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent e){
+        if (!e.getPlayer().hasPermission("doublejump.use")) return;
+        if (!e.getPlayer().isOnGround()) return;
+        cooldown.replace(e.getPlayer().getUniqueId(), false);
+    }
+
+    @EventHandler
+    public void onGamemodeChange(PlayerGameModeChangeEvent e){
+        if (!e.getPlayer().hasPermission("doublejump.use")) return;
+        if (e.getNewGameMode() == GameMode.SURVIVAL){
+            e.getPlayer().setAllowFlight(true);
+        }
+    }
+
 }
