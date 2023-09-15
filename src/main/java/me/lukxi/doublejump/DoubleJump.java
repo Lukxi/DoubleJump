50f694d34d0bddcfe118af7b900fd2dd13340ab2
Lukxi <nowak.lukas03@outlook.de>
2023-09-16 01:36:51 +0200

    register listener in on enable

--- src/main/java/me/lukxi/doublejump/DoubleJump.java
+++ src/main/java/me/lukxi/doublejump/DoubleJump.java
@@ -1,8 +1,14 @@
 package me.lukxi.doublejump;
 
+
+import me.lukxi.doublejump.listeners.JumpListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DoubleJump extends JavaPlugin {
 
-
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(new JumpListener(), this);
+    }
 }
